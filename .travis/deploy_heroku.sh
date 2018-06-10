f8d120ed44570dc90f4fbcf88c7745aa99ac9438
linjun <lj@terminus.io>
2018-06-10 17:11:50 +0800

    heroku: fix app name

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -3,4 +3,4 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:release web --app $HEROKU_APP_NAME
-heroku ps:scale web=1
+heroku ps:scale web=1 --app $HEROKU_APP_NAME
