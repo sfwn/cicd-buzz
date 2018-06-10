e3046cf812411ac5b977059d5309dad7dc6202a8
linjun <lj@terminus.io>
2018-06-10 17:06:42 +0800

    heroku scale 1

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -3,3 +3,4 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:release web --app $HEROKU_APP_NAME
+heroku ps:scale web=1
